b00f0bafb097cee0e87e1db9c9cbc0f380244e35
Varun Singh <varun7singh10@gmail.com>
2022-03-26 23:46:35 +0530

    a

---
+++ app.js
@@ -0,0 +1,60 @@
+var inputval = document.getElementById("inputval");
+var btn = document.getElementById("btn");
+var todolist = document.getElementById("todolist");
+let c = 0;
+function add(){
+    if (inputval.value == "") {
+        return;
+      }
+      var task = document.createElement("div");
+      var deletebtn = document.createElement("i");
+      var checker = document.createElement("i");
+    
+      task.setAttribute("class", "task");
+      deletebtn.setAttribute("class", "fa-2x fa fa-trash");
+      checker.setAttribute("class", "fa-2x fa-regular fa-square");
+    
+      var text = document.createElement("p");
+      text.setAttribute("class", "ptask");
+      text.innerText = inputval.value;
+    
+      todolist.appendChild(task);
+      task.appendChild(checker);
+      task.appendChild(text);
+      task.appendChild(deletebtn);
+    
+      deletebtn.addEventListener("click", e=> {
+        task.remove();
+      })
+      task.addEventListener("click", e=> {
+          if(c%2==0){
+            checker.classList.remove("fa-2x","fa-regular","fa-square")
+            checker.classList.add("fa-2x","fa-solid" ,"fa-square-check")
+            c++;
+          }
+          else{
+            checker.classList.remove("fa-2x","fa-solid" ,"fa-square-check")
+            checker.classList.add("fa-2x","fa-regular","fa-square")
+            c++;
+          }
+         
+      })
+      
+    
+      inputval.value = ""; //to clear placeholder
+}
+function control(e){
+    if(e.keyCode === 13)
+    {
+        add();
+    }
+}
+
+
+document.addEventListener("keydown", control)
+
+btn.addEventListener("click", (e) => {
+  add()
+});
+
+
